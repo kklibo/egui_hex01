5c39137be8e43b8dc66f287e39903b290b607ba9
kklibo <kklibo4@gmail.com>
2024-06-27 10:06:01 -0700

    test scrollarea

--- src/hex_app.rs
+++ src/hex_app.rs
@@ -1,3 +1,5 @@
+use egui::style::ScrollStyle;
+
 pub struct HexApp {}
 
 impl Default for HexApp {
@@ -12,5 +14,49 @@ impl HexApp {
     }
 }
 impl eframe::App for HexApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {}
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("hex diff test (egui UI)");
+
+            ui.spacing_mut().scroll = ScrollStyle::solid();
+            egui::ScrollArea::vertical()
+                .scroll_bar_visibility(egui::scroll_area::ScrollBarVisibility::AlwaysVisible)
+                .show(ui, |ui| {
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                    ui.label("123132");
+                });
+        });
+    }
 }
