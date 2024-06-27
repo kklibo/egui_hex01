4688cbcefbfa5593fa61bb6896a847c6f113d286
kklibo <kklibo4@gmail.com>
2024-06-27 11:51:59 -0700

    address column + nested Column test

--- src/hex_app.rs
+++ src/hex_app.rs
@@ -25,6 +25,19 @@ impl HexApp {
     }
 }
 
+fn display_address_column(egui_id: &str, ui: &mut Ui, rows: usize) {
+    let hex_width = 16;
+    egui::Grid::new(egui_id)
+        .spacing(Vec2::new(-20f32, 0f32))
+        .striped(true)
+        .show(ui, |ui| {
+            (0..rows).for_each(|c| {
+                ui.label(format!("{:08X}", c * hex_width));
+                ui.end_row();
+            });
+        });
+}
+
 fn display_hex_field(egui_id: &str, ui: &mut Ui, bytes: &[u8]) {
     let width = 16;
     egui::Grid::new(egui_id)
@@ -51,11 +64,20 @@ impl eframe::App for HexApp {
                 .show(ui, |ui| {
                     ui.columns(2, |columns| {
                         columns[0].label("First column");
-                        display_hex_field(
-                            "pattern0 hex",
-                            &mut columns[0],
-                            &self.pattern0.as_ref().unwrap().data,
-                        );
+
+                        columns[0].columns(2, |columns| {
+                            display_address_column(
+                                "pattern0 addresses",
+                                &mut columns[0],
+                                1 + &self.pattern0.as_ref().unwrap().data.len() / 16,
+                            );
+                            display_hex_field(
+                                "pattern0 hex",
+                                &mut columns[1],
+                                &self.pattern0.as_ref().unwrap().data,
+                            );
+                        });
+
                         columns[1].label("Second column");
                         display_hex_field(
                             "pattern1 hex",
