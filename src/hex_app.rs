96724377ef8a42d0726ec4105ae083d253b9102b
kklibo <kklibo4@gmail.com>
2024-06-28 10:06:53 -0700

    begin add_body_contents: Table row loading

--- src/hex_app.rs
+++ src/hex_app.rs
@@ -1,6 +1,6 @@
 use egui::style::ScrollStyle;
 use egui::{Ui, Vec2};
-use egui_extras::{Column, TableBuilder};
+use egui_extras::{Column, TableBody, TableBuilder};
 
 struct Pattern {
     name: String,
@@ -54,6 +54,42 @@ fn display_hex_field(egui_id: &str, ui: &mut Ui, bytes: &[u8]) {
         });
 }
 
+impl HexApp {
+    fn add_body_contents(&self, mut body: TableBody<'_>) {
+        let hex_grid_width = 16;
+
+        let mut addresses0 = (0..).map(|c| format!("{:08X}", c * hex_grid_width));
+
+        let mut pattern0 = if let Some(pattern) = &self.pattern0 {
+            pattern.data.iter()
+        } else {
+            [].iter()
+        }
+        .map(|&b| format!("{b:02X}"));
+
+        let row_height = 18.0;
+        let num_rows = 10_000;
+        body.rows(row_height, num_rows, |mut row| {
+            let row_index = row.index();
+            row.col(|ui| {
+                ui.label(addresses0.next().expect("iterator should be infinite"));
+            });
+            row.col(|ui| {
+                egui::Grid::new(format!("pattern0row{row_index}"))
+                    .spacing(Vec2::new(-20f32, 0f32))
+                    .striped(true)
+                    .show(ui, |ui| {
+                        (0..hex_grid_width).for_each(|_| {
+                            let s = pattern0.next().unwrap_or_else(|| "__".to_string());
+                            ui.label(s);
+                        });
+                        ui.end_row();
+                    });
+            });
+        });
+    }
+}
+
 impl eframe::App for HexApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -74,31 +110,7 @@ impl eframe::App for HexApp {
                         ui.heading("hex1");
                     });
                 })
-                .body(|mut body| {
-                    body.row(30.0, |mut row| {
-                        row.col(|ui| {
-                            display_address_column(
-                                "pattern0 addresses",
-                                ui,
-                                1 + &self.pattern0.as_ref().unwrap().data.len() / 16,
-                            );
-                        });
-                        row.col(|ui| {
-                            display_hex_field(
-                                "pattern0 hex",
-                                ui,
-                                &self.pattern0.as_ref().unwrap().data,
-                            );
-                        });
-                        row.col(|ui| {
-                            display_hex_field(
-                                "pattern1 hex",
-                                ui,
-                                &self.pattern1.as_ref().unwrap().data,
-                            );
-                        });
-                    });
-                });
+                .body(|body| self.add_body_contents(body));
         });
     }
 }
