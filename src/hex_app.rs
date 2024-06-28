662b5cfbc43179dae5b4189d0a2f62ce027bd5c6
kklibo <kklibo4@gmail.com>
2024-06-28 15:09:40 -0700

    add randomize buttons

--- src/hex_app.rs
+++ src/hex_app.rs
@@ -1,11 +1,17 @@
 use egui::Ui;
 use egui_extras::{Column, TableBody, TableBuilder, TableRow};
+use rand::Rng;
 
 pub struct HexApp {
     source_name0: Option<String>,
     source_name1: Option<String>,
     pattern0: Option<Vec<u8>>,
     pattern1: Option<Vec<u8>>,
+}
+
+fn random_pattern() -> Vec<u8> {
+    let mut rng = rand::thread_rng();
+    (0..1000).map(|_| rng.gen_range(0..=255)).collect()
 }
 
 impl HexApp {
@@ -17,20 +23,26 @@ impl HexApp {
             pattern1: Some(vec![0; 1000]),
         }
     }
-}
 
-impl HexApp {
-    fn add_header_row(&self, mut header: TableRow<'_, '_>) {
+    fn add_header_row(&mut self, mut header: TableRow<'_, '_>) {
         let no_pattern = "[none]".to_string();
 
         header.col(|ui| {
             ui.heading("address");
         });
         header.col(|ui| {
-            ui.heading(self.source_name0.as_ref().unwrap_or_else(|| &no_pattern));
+            ui.heading(self.source_name0.as_ref().unwrap_or(&no_pattern));
+            if ui.button("randomize").clicked() {
+                self.pattern0 = Some(random_pattern());
+                self.source_name0 = Some("random".to_string());
+            }
         });
         header.col(|ui| {
-            ui.heading(self.source_name1.as_ref().unwrap_or_else(|| &no_pattern));
+            ui.heading(self.source_name1.as_ref().unwrap_or(&no_pattern));
+            if ui.button("randomize").clicked() {
+                self.pattern1 = Some(random_pattern());
+                self.source_name1 = Some("random".to_string());
+            }
         });
     }
 
